8a82136c84331e5164af14ca205e101649d89469
茗香墨语 <yaowen424@126.com>
2015-10-29 16:31:09 +0800

    抛异常的线程处理

--- app/src/main/java/com/yaowen/androidthreadtest/MainActivity.java
+++ app/src/main/java/com/yaowen/androidthreadtest/MainActivity.java
@@ -2,12 +2,31 @@ package com.yaowen.androidthreadtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button changeText;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changeText= (Button) findViewById(R.id.change_text);
+        textView= (TextView) findViewById(R.id.text);
+
+        changeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable(){
+
+                    @Override
+                    public void run() {
+                        textView.setText("Nice to meet you!");
+                    }
+                }).start();
+            }
+        });
     }
 }
